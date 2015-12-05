d5872ba4821419f81f55edffbe05c60c84de03d3
cecekpawon <killprie@gmail.com>
2015-12-06 02:55:39 +0700

    re-init

--- javascripts/responsive-paginate.js
+++ javascripts/responsive-paginate.js
@@ -5,180 +5,207 @@
 
 (function ($){
     jQuery.fn.rPage = function () {
-        var $this = $(this);
-        for(var i = 0, max = $this.length; i < max; i++)
-        {
-        	new rPage($($this[i]));
-        }
+        $(this).each(function(){
+            new rPage($(this));
+        });
 
         function rPage($container)
         {
-        	this.label = function()
-        	{
-        		var active_index = this.els.filter(".active").index();
-        		var rp = this;
-        		this.els.each(function(){
-        			if (rp.isNextOrPrevLink($(this)) == false)
-        			{
-        				$(this).addClass("page-away-" + (Math.abs(active_index - $(this).index())).toString());
-        			}
-        			else
-        			{
-        				if ($(this).index() > active_index)
-        				{
-        					$(this).addClass("right-etc");
-        				}
-        				else
-        				{
-        					$(this).addClass("left-etc");
-        				}
-        			}
-        		});
-        	}
+            this.container = $container;
+            this.parent = this.container.parent();
+            // maybe user can pre-define this later
+            this.nextprev_class = ["prev", "next"];
+            this.nextprev_text = $.merge(this.nextprev_class, ["»", "«"]);
+            this.dots_class = ["dots"];
+            this.dots_text = ["..."];
+            this.disabled_class = "disabled removable";
+            this.rightetc_class = "right-etc";
+            this.leftetc_class = "left-etc";
+            this.pageaway_class = "page-away-";
+            this.parsed_class = "parsed";
+            this.active_class = "active";
+            this.child_el = "li";
+            this.child_el_inner = "span";
 
-        	this.makeResponsive = function()
-    	    {
-    	    	this.reset();
-    	    	var width = this.calculateWidth();
+            this.label = function()
+            {
+                var
+                   active_index = this.els.filter("." + this.active_class).index(),
+                   $this = this,
+                   className = "";
 
-    	    	while (width > this.els.parent().parent().outerWidth() - 10)
-    	    	{
-    	    		var did_remove = this.removeOne();
-    	    		if (did_remove == false)
-    	    		{
-    	    			break;
-    	    		}
-    	    		width = this.calculateWidth();
-    	    	}
-    	    }
+                this.els.each(function(){
+                    if (!$this.isNextOrPrevLink($(this)))
+                    {
+                        className = $this.pageaway_class + (Math.abs(active_index - $(this).index())).toString();
+                    }
+                    else
+                    {
+                        className = ($(this).index() > active_index) ? this.rightetc_class : this.leftetc_class;
+                    }
+                    $(this).addClass(className);
+                });
+            }
 
-        	this.isNextOrPrevLink = function(element)
-        	{
+            this.compareText = function(text, str)
+            {
+                if (!text) return;
+
+                var regex = new RegExp(
+                        "("
+                        + str
+                            .join("@")
+                            .replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&")
+                            .replace(/@/g, "|")
+                        + ")",
+                        "gi"
+                    );
+
+                return text.match(regex);
+            }
+
+            this.makeResponsive = function()
+            {
+                this.reset();
+
+                while (this.container.outerWidth() > (this.parent.outerWidth() - 10))
+                {
+                    if (!this.removeOne())
+                    {
+                        break;
+                    }
+                }
+            }
+
+            this.isNextOrPrevLink = function(element)
+            {
                 return (
-                    element.hasClass('pagination-prev')
-                    || element.hasClass('pagination-next')
-                    || element.text() == "»"
-                    || element.text() == "«"
-                );
-        	}
+                        this.compareText(element.attr("class"), this.nextprev_class)
+                        || this.compareText(element.text(), this.nextprev_text)
+                    );
+            }
 
-        	this.isRemovable = function(element)
-        	{
-        		if (this.isNextOrPrevLink(element))
-        		{
-        			return false;
-        		}
-        		var index = this.els.filter(element).index();
-        		if (index == 1 || this.isNextOrPrevLink($container.find("li").eq(index + 1)))
-        		{
-        			return false;
-        		}
-        		if (element.text() == "...")
-        		{
-        			return false;
-        		}
-        		return true;
-        	}
+            this.isRemovable = function(element)
+            {
+                if (this.isNextOrPrevLink(element))
+                {
+                    return false;
+                }
+                var index = this.els.filter(element).index();
+                if (index == 1 || this.isNextOrPrevLink(this.container.find(this.child_el).eq(index + 1)))
+                {
+                    return false;
+                }
+                return (
+                        !this.compareText(element.attr("class"), this.dots_class)
+                        && !this.compareText(element.text(), this.dots_text)
+                    );
+            }
 
-    	    this.removeOne = function()
-    	    {
-    	    	var active_index = this.els.filter(".active").index();
-    	    	var farthest_index = $container.find("li").length - 1;
-    	    	var next = active_index + 1;
-    	    	var prev = active_index - 1;
+            this.removeOne = function()
+            {
+                var
+                    active_index = this.els.filter("." + this.active_class).index(),
+                    li = $container.find(this.child_el).not("." + this.parsed_class),
+                    farthest_index = li.length > 0 ? li.length - 1 : 0,
+                    is_needsEtcSign_init = false,
+                    is_needsEtcSign_prev = this.needsEtcSign(1, active_index),
+                    is_needsEtcSign_next = this.needsEtcSign(active_index, farthest_index/* - 1*/),
+                    exitloop = false,
+                    htmlEtcSign = $("<" + this.child_el + "/>", {class: this.disabled_class})
+                        .append($("<" + this.child_el_inner + "/>", {text: this.dots_text})),
+                    $this = this;
 
-    	    	for (var i = farthest_index - 1; i > 0; i--)
-    	    	{
-    	    		var candidates = this.els.filter(".page-away-" + i.toString());
-    	    		var candidate = candidates.filter(function(){
-    	    			return this.style["display"] != "none";
-    	    		});
-    	    		if (candidate.length > 0)
-    	    		{
-    	    			for (var j = 0; j < candidate.length; j++)
-    	    			{
-    	    				var candid_candidate = candidate.eq(j);
-    	    				if (this.isRemovable(candid_candidate))
-    		    			{
-    			    			candid_candidate.css("display", "none");
-    			    			if (this.needsEtcSign(active_index, farthest_index - 1))
-    			    			{
-    			    				this.els.eq(farthest_index - 2).before("<li class='disabled removable'><span>...</span></li>");
-    			    			}
-    			    			if (this.needsEtcSign(1, active_index))
-    			    			{
-    			    				this.els.eq(1).after("<li class='disabled removable'><span>...</span></li>");
-    			    			}
-    			    			return true;
-    		    			}
-    	    			}
-    	    		}
-    	    	}
-    	    	return false;
-    	    }
+                for (var i = farthest_index/* - 1*/; i > 0; i--)
+                {
+                    var candidate = $this.els.not("." + $this.parsed_class).filter(function(){
+                        return $(this).hasClass($this.pageaway_class + i.toString())/* && this.style["display"] != "none"*/;
+                    });
 
-    	    this.needsEtcSign = function(el1_index, el2_index)
-    	    {
-    	    	if (el2_index - el1_index <= 1)
-    	    	{
-    	    		return false;
-    	    	}
-    	    	else
-    	    	{
-    	    		var hasEtcSign = false;
-    	    		var hasHiddenElement = false;
-    	    		for (var i = el1_index + 1; i < el2_index; i++)
-    	    		{
-    	    			var el = $container.find("li").eq(i);
-    	    			if (el.css("display") == "none")
-    	    			{
-    	    				hasHiddenElement = true;
-    	    			}
-    	    			if (el.text() == "...")
-    	    			{
-    	    				hasEtcSign = true;
-    	    			}
-    	    		}
-    	    		if (hasHiddenElement == true && hasEtcSign == false)
-    	    		{
-    	    			return true;
-    	    		}
-    	    	}
-    	    	return false;
-    	    }
+                    candidate.each(function(){
+                        var candid_candidate = $(this);
+                        if ($this.isRemovable(candid_candidate)) {
+                            candid_candidate
+                                .addClass($this.parsed_class)
+                                .hide();
 
-    	    this.reset = function()
-    	    {
-    	    	for (var i = 0; i < this.els.length; i++)
-    	    	{
-    	    		this.els.eq(i).css("display", "inline");
-    	    	}
-    	    	$container.find("li").filter(".removable").remove();
-    	    }
+                            if (!is_needsEtcSign_init) {
+                                is_needsEtcSign_init = true;
 
-    	    this.calculateWidth = function()
-    	    {
-    	    	var width = 0;
-    	    	for (var i = 0; i < $container.find("li").length; i++)
-    	    	{
-    	    		width += $container.find("li").eq(i).children("a").eq(0).outerWidth();
-    	    		width += $container.find("li").eq(i).children("span").eq(0).outerWidth();
-    	    	}
-    	    	return width;
-    	    }
+                                if (is_needsEtcSign_next)
+                                {
+                                    $this.els.eq(farthest_index - 2).before(htmlEtcSign);
+                                }
+                                if (is_needsEtcSign_prev)
+                                {
+                                    $this.els.eq(1).after(htmlEtcSign);
+                                }
+                            }
 
-    	    this.els = $container.find("li");
-    	    this.label();
-    	    this.makeResponsive();
+                            exitloop = true;
+                            return exitloop;
+                        }
+                    });
 
-    	    var resize_timer;
+                    if (!exitloop) continue;
+                    return exitloop;
+                }
+            }
+
+            this.needsEtcSign = function(el1_index, el2_index)
+            {
+                if (el2_index - el1_index <= 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    var
+                        slice = this.container.find(this.child_el).slice(el1_index, el2_index),
+                        hasHiddenElement = slice.find(":hidden").length,
+                        //hasHiddenElement = slice.find("." + this.parsed_class).length,
+                        textmap = $.map(
+                          slice,
+                          function(el) {
+                              return $(el).text()
+                          })
+                          .join(" "),
+                        hasEtcSign = slice.find("[class*='" + this.dots_class + "']").length || this.compareText(textmap, this.dots_text);
+
+                    return (hasHiddenElement && !hasEtcSign);
+                }
+            }
+
+            this.reset = function()
+            {
+                $(this.els)
+                    .removeClass(this.parsed_class)
+                    .css("display", "inline")
+                    .filter(".removable")
+                        .remove();
+            }
+
+            this.calculateWidth = function()
+            {
+                return this.container.outerWidth();
+            }
+
+            this.els = this.container.find(this.child_el);
+            this.label();
+            this.makeResponsive();
+
+            var resize_timer;
 
             $(window).resize(
-            	$.proxy(function()
-            	{
-            		clearTimeout(resize_timer);
-            		resize_timer = setTimeout($.proxy(function(){this.makeResponsive()}, this), 100);
-            	}, this)
+                $.proxy(function()
+                {
+                    // realtime, to avoid delay
+                    this.makeResponsive();
+                    /*
+                    clearTimeout(resize_timer);
+                    resize_timer = setTimeout($.proxy(function(){this.makeResponsive()}, this), 100);
+                    */
+                }, this)
             );
         }
     };
